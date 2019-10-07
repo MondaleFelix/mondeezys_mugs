b0e929e4ddbd66ddd33ddd79c1e26e2d97d31a6d
MondaleFelix <mondalefelix@yahoo.com>
2019-10-07 15:13:11 -0700

    users can now edit their mugs

--- app.py
+++ app.py
@@ -36,8 +36,24 @@ def mugs_show(mug_id):
 
 @app.route("/mugs/new")
 def mugs_new():
-	return render_template("mugs_new.html")
+	return render_template("mugs_new.html", mug ={}, title ="New Mug")
 
+@app.route("/mugs/<mug_id>/edit")
+def mugs_edit(mug_id):
+	mug = mugs.find_one({"_id" : ObjectId(mug_id)})
+	return render_template("mugs_edit.html", mug = mug, title = "Edit Mug")
+
+@app.route("/mugs/<mug_id>", methods = ['POST'])
+def mugs_update(mug_id):
+	updated_mug = {
+		"mug_name": request.form.get("mug_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+		"color": request.form.get("color")
+	}
+
+	mugs.update_one( {"_id" : ObjectId(mug_id)}, {"$set" : updated_mug})
+	return redirect(url_for("mugs_show", mug_id = mug_id))
 
 if __name__ == "__main__":
 	app.run(debug=True)
