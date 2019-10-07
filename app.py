954eb996b96f7f7955d61f70333483ca21b3b9e0
MondaleFelix <mondalefelix@yahoo.com>
2019-10-07 01:19:35 -0700

    users can add a new mug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -15,6 +15,23 @@ def mugs_index():
 	# Shows all mugs
 	return render_template("mugs_index.html", mugs=mugs.find())
 
+@app.route("/mugs", methods=["POST"])
+def playlists_submit():
+	mug = {
+		"mug_name": request.form.get("mug_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+		"color": request.form.get("color")
+
+	}
+	print(mug)
+	mugs.insert_one(mug)
+	return redirect(url_for("mugs_index"))
+
+
+@app.route("/mugs/new")
+def mugs_new():
+	return render_template("mugs_new.html")
 
 
 if __name__ == "__main__":
