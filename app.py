26221744d96f06649d4f2cee70ecdafb16eea6c5
MondaleFelix <mondalefelix@yahoo.com>
2019-10-06 21:03:56 -0700

    Users can now see them mugs

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+mugs = [
+	{ "product_name" : "Mondale's Mug", 'price' : 2}
+]
+
 @app.route("/")
-def index():
-	# Return homepage
-	return render_template('home.html', msg="Modeezy's Mugs")
+def mugs_index():
+	# Shows all mugs
+	return render_template("mugs_index.html", mugs=mugs)
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
